1d457170686a68ead997f8fe164ff1a3800b8106
gavreg <faktorial5@gmail.com>
2026-04-23 15:20:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,369 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Reflection.Metadata;
+using ConsoleApp11;
+using RtfDomParser;
+using OfficeOpenXml;
+
+
+string inputfile="";
+
+for (int i = 0; i<args.Length; i++)
+{
+    switch (args[i])
+    {
+        case "-i":
+            {
+                inputfile = args[++i];
+                 
+
+                break;
+            }
+    }
+}
+
+FileInfo fi = new FileInfo(inputfile);
+
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+
+const int curYear = 25;
+
+RTFDomDocument doc = new RTFDomDocument();
+var nagr = File.ReadAllText(inputfile);
+doc.LoadRTFText(nagr);
+
+
+var rows = new List<RTFDomTableRow>();
+
+
+
+var discipline_rows = new List<Discipline_Row>();
+var va_rows = new List<VARow>();
+
+
+for (int i = 0; i < doc.Elements.Count; ++i)
+{
+    var rootable = doc.Elements[i];
+    for (int t = 0; t < rootable.Elements.Count; ++t)
+    {
+        var el = rootable.Elements[t];
+        var row = el as RTFDomTableRow;
+        functions.PrintRow(row);
+    }
+}
+
+Console.WriteLine(new String('=', 10));
+
+for (int i = 0; i < doc.Elements.Count; ++i)
+{
+    var rootable = doc.Elements[i];
+    for (int t = 0; t < rootable.Elements.Count; ++t)
+    {
+
+
+        var el = rootable.Elements[t];
+        var row = el as RTFDomTableRow;
+
+        string row_string = functions.GetRowText(row);
+
+        
+
+        //ищем строку с аудиторной нагрузкой
+        if (row.Elements.Count >= 54 &&
+                Int32.TryParse(functions.GetCellText(row.Elements[Discipline_Row.fields_map[Discipline_Row.Fields.ID]] as RTFDomTableCell), out _) &&
+                !string.IsNullOrEmpty(functions.GetCellText(row.Elements[Discipline_Row.fields_map[Discipline_Row.Fields.NAME]] as RTFDomTableCell)) &&
+                Double.TryParse(functions.GetCellText(row.Elements[Discipline_Row.fields_map[Discipline_Row.Fields.SUM]] as RTFDomTableCell), out _)
+            )
+        {
+            
+            var d_row = new Discipline_Row();
+            discipline_rows.Add(d_row);
+
+            foreach (var x in Discipline_Row.fields_map)
+            {
+                string data = functions.GetCellText(row.Elements[x.Value] as RTFDomTableCell);
+                if (data != "")
+                    d_row[x.Key] += " " + data;
+            }
+
+            d_row.PrintFormatted();
+
+        }
+        //ищем строку с внеаудиторной нагрузкой
+        if (row.Elements.Count >= 30 &&
+                Int32.TryParse(functions.GetCellText(row.Elements[VARow.fields_map[VARow.Fields.ID]] as RTFDomTableCell), out _) &&
+                !string.IsNullOrEmpty(functions.GetCellText(row.Elements[VARow.fields_map[VARow.Fields.NAME]] as RTFDomTableCell)) &&
+                Double.TryParse(functions.GetCellText(row.Elements[VARow.fields_map[VARow.Fields.HOURS]] as RTFDomTableCell), out _)
+            )
+
+        {
+            
+
+            var varow = new VARow();
+            va_rows.Add(varow);
+
+            foreach (var x in VARow.fields_map)
+            {
+                string data = functions.GetCellText(row.Elements[x.Value] as RTFDomTableCell);
+                if (data != "")
+                    varow[x.Key] += " " + data;
+            }
+
+            varow.PrintFormatted();
+        }
+
+    }
+}
+
+Console.WriteLine(new String('=', 10));
+
+
+using StreamWriter sw = new StreamWriter($"{Path.GetFileNameWithoutExtension(fi.Name)}.txt",false);
+using MemoryStream ms = new MemoryStream(sw);
+
+
+
+sw.WriteLine($"Всего аудиторной:");
+double sum_os = Math.Round(discipline_rows.Where(x =>  x.sem.Value % 2 != 0).Sum(x => x.sum.Value),1);
+double sum_ves = Math.Round(discipline_rows.Where(x => x.sem.Value % 2 == 0).Sum(x => x.sum.Value),1);
+double sum = Math.Round(sum_os + sum_ves, 1);
+sw.WriteLine($"Осень: {sum_os}");
+sw.WriteLine($"Весна: {sum_ves}");
+sw.WriteLine($"Всего: {sum}");
+
+sw.WriteLine($"");
+
+var excel = new ExcelPackage();
+var ws = excel.Workbook.Worksheets.Add("Аудиторная");
+
+
+for (int i=1; i<30; ++i)
+{
+    ws.Cells[1, i].Value = i;
+}
+
+var ___i = 1;
+ws.Cells[2, ___i++].Value = "id";
+ws.Cells[2, ___i++].Value = "наим";
+ws.Cells[2, ___i++].Value = "сем";
+ws.Cells[2, ___i++].Value = "пот";
+ws.Cells[2, ___i++].Value = "пот2";
+ws.Cells[2, ___i++].Value = "гр";
+ws.Cells[2, ___i++].Value = "подгр";
+
+ws.Cells[2, ___i++].Value = "лек кол";
+ws.Cells[2, ___i++].Value = "лек час";
+ws.Cells[2, ___i++].Value = "лек бю";
+
+ws.Cells[2, ___i++].Value = "сем кол";
+ws.Cells[2, ___i++].Value = "сем час";
+ws.Cells[2, ___i++].Value = "сем студ";
+ws.Cells[2, ___i++].Value = "сем инд";
+
+ws.Cells[2, ___i++].Value = "лаб кол";
+ws.Cells[2, ___i++].Value = "лаб час";
+ws.Cells[2, ___i++].Value = "лаб студ";
+ws.Cells[2, ___i++].Value = "лаб инд";
+
+ws.Cells[2, ___i++].Value = "кур/чел";
+ws.Cells[2, ___i++].Value = "кур час";
+
+ws.Cells[2, ___i++].Value = "инд/чел";
+ws.Cells[2, ___i++].Value = "инд час";
+
+ws.Cells[2, ___i++].Value = "всего";
+
+
+
+
+int _row = 3; 
+
+foreach (var dr in discipline_rows)
+{
+    sw.WriteLine($"{dr.id} {dr.name}");
+    sw.WriteLine($"   Семестр {dr.sem}");
+    sw.WriteLine($"   Поток {dr.stream}");
+    sw.WriteLine($"   Кол-во групп {dr.group} ({dr.sub_group} подогр.)");
+    sw.WriteLine($"   Студентов {dr.student}");
+
+    var __i = 1;
+    int _sem = dr.sem.Value;
+    ws.Cells[_row, __i++].Value = dr.id;
+    ws.Cells[_row, __i++].Value = dr.name;
+    ws.Cells[_row, __i++].Value = dr.sem;
+    ws.Cells[_row, __i++].Value = dr.stream;
+    ws.Cells[_row, __i++].Value = dr.stream + "-" + Math.Round(1.0*curYear - ( (_sem - 1))%8 / 2).ToString();
+    ws.Cells[_row, __i++].Value = dr.group;
+    ws.Cells[_row, __i++].Value = dr.student;
+
+   
+
+    if (dr.lections.HasValue)
+    {
+        double lek = dr.lections.Value ;
+        
+        sw.WriteLine($"   ЛЕКЦИИ -> Кол-во      {lek / 2}    ");
+        sw.WriteLine($"             Часов-всего {lek}");
+        sw.WriteLine($"             Индив.      0,3");
+
+        ws.Cells[_row, __i++].Value = lek / 2;
+        ws.Cells[_row, __i++].Value = lek;
+        ws.Cells[_row, __i++].Value = lek;
+    }
+    else    
+        __i+=3;
+
+
+    if (dr.pr.HasValue)
+    {
+        double pr = dr.pr.Value / dr.group.Value;
+        double pr_all = dr.pr.Value;
+        double ind = 0;
+        if (dr.sem < 5)
+        {
+            ind = 0.5 * pr / 8.0;
+        }
+        else
+        {
+            ind = 0.2 * pr / 4.0;
+        }
+        sw.WriteLine($"   СЕМИНАРЫ -> Кол-во      {pr / 2}   ");
+        sw.WriteLine($"               Часов-всего {pr_all}");
+        sw.WriteLine($"               Индив.      {Math.Round(ind, 1, MidpointRounding.ToZero)}");
+
+        ws.Cells[_row, __i++].Value = pr / 2;
+        ws.Cells[_row, __i++].Value = pr_all;
+        ws.Cells[_row, __i++].Value = pr_all;
+        ws.Cells[_row, __i++].Value = Math.Round(ind, 1, MidpointRounding.ToZero);
+
+    }
+    else
+        __i += 4;
+
+    if (dr.labs.HasValue)
+    { 
+        double labs = Math.Round(dr.labs.Value / dr.sub_group.Value, 1);
+
+        double labs_all = dr.labs.Value;
+        double ind = 0;
+        if (dr.sem < 5)
+        {
+            ind = 0.3 * labs / 4.0;
+        }
+        else
+        {
+            ind = 0.2 * labs / 4.0;
+        }
+        sw.WriteLine($"   ЛАБОРАТОРНЫЕ -> Кол-во      {labs / 4} ");
+        sw.WriteLine($"                   Часов-всего {labs_all}");
+        sw.WriteLine($"                   Индив.      {Math.Round(ind,1, MidpointRounding.ToZero)}");
+
+        ws.Cells[_row, __i++].Value = labs / 4;
+        ws.Cells[_row, __i++].Value = labs_all;
+        ws.Cells[_row, __i++].Value = labs_all;
+        ws.Cells[_row, __i++].Value = Math.Round(ind, 1, MidpointRounding.ToZero);
+    }
+    else
+        __i += 4;
+
+    if (dr.kur.HasValue)
+    {
+        double kur = dr.kur.Value;
+        double hours_per_man = kur / dr.student.Value;
+        sw.WriteLine($"   КУРСОВЫЕ ->     На человека: {hours_per_man} ");
+        sw.WriteLine($"                   Часов-всего  {kur}");
+
+        ws.Cells[_row, __i++].Value = hours_per_man;
+        ws.Cells[_row, __i++].Value = kur;
+    }
+    else
+        __i += 2;
+
+    if (dr.ind > 0)
+    {
+        double ind = dr.ind.Value;
+        double ind_per_man = ind / dr.student.Value;
+        sw.WriteLine($"   ИНДИВИДУАЛЬНАЯ -> На человека: {ind_per_man}");
+        sw.WriteLine($"                     Часов-всего  {ind}");
+
+        ws.Cells[_row, __i++].Value = ind_per_man;
+        ws.Cells[_row, __i++].Value = ind;
+    }
+    else
+        __i += 2;
+    sw.WriteLine($"   ВСЕГО ЗА КУРС ->  {dr.sum}");
+    ws.Cells[_row, __i++].Value = dr.sum;
+
+    sw.WriteLine("--------------");
+    sw.WriteLine("");
+
+    _row++;
+
+}
+
+ws = excel.Workbook.Worksheets.Add("Внеаудиторная");
+
+_row = 3;
+
+va_rows = va_rows.Where(x => (x[VARow.Fields.STREAM] != null) && !x[VARow.Fields.STREAM].Contains("АСП")).ToList();
+
+___i = 1;
+
+ws.Cells[2, ___i++].Value = "id";
+ws.Cells[2, ___i++].Value = "имя";
+ws.Cells[2, ___i++].Value = "сем";
+ws.Cells[2, ___i++].Value = "пот";
+ws.Cells[2, ___i++].Value = "пот2";
+ws.Cells[2, ___i++].Value = "груп";
+ws.Cells[2, ___i++].Value = "студ";
+ws.Cells[2, ___i++].Value = "ф-ла";
+ws.Cells[2, ___i++].Value = "часы";
+
+foreach (var _varow in va_rows)
+{
+    int __i = 1;
+
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.ID];
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.NAME];
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.SEM];
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.STREAM];
+    //  --->>>   dr.stream + "-" + Math.Round(1.0*curYear - ( (_sem - 1))%8 / 2).ToString();
+    int _sem = 0;
+    if ( Int32.TryParse(_varow[VARow.Fields.SEM],out _sem) )
+    {
+        ws.Cells[_row, __i++].Value = _varow[VARow.Fields.STREAM] + "-" + Math.Round(1.0 * curYear - ((_sem - 1)) % 8 / 2).ToString();
+    }
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.GROUPS];
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.STUDENTS];
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.HOURS_FORMULA];
+    ws.Cells[_row, __i++].Value = _varow[VARow.Fields.HOURS];
+
+    _row++;
+
+}
+
+sw.Close();
+
+
+
+
+
+for(bool f = true; f;)
+{
+    try
+    {
+        excel.SaveAs($"{Path.GetFileNameWithoutExtension(fi.Name)}.xlsx");
+        f = false;
+    }
+
+    catch (Exception e)
+    {        
+
+        Console.WriteLine(e.ToString());
+        Console.WriteLine("");
+        Console.WriteLine("нажмите клавишу для повтора...");
+        Console.ReadKey();
+    }
+}
+
---
+++ functions.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using RtfDomParser;
+
+namespace ConsoleApp11
+{
+    enum Mode { SEARCHING, AUD_PARCE, VA_PARCE }
+
+    public static class functions
+    {
+        public static string GetCellText(RTFDomTableCell? cell)
+        {
+            if (cell == null) return "";
+            if (cell.Elements.Count == 0) return "";
+            if (cell.Elements[0].Elements.Count == 0) return "";
+            string ret="";
+            for (int i = 0; i < cell.Elements[0].Elements.Count; ++i)
+            {
+                var e = cell.Elements[0].Elements[i];
+                ret += e.InnerText.Replace("?", "");
+
+            }
+            return ret;
+        }
+
+        public static string GetRowText(RTFDomTableRow? row)
+        {
+            if (row == null) return "";
+            string ret = "|";
+
+            for (int i=0; i<row.Elements.Count; ++i)
+            {
+                ret += GetCellText(row.Elements[i] as RTFDomTableCell);
+                i += (row.Elements[i] as RTFDomTableCell).RowSpan;
+            }
+            return ret;
+            
+        }
+
+
+        public static void PrintRow(RTFDomTableRow? row)
+        {
+            bool first = true;
+
+            List<string> cell_strings = new List<string>();
+            List<int> cell_lengths = new List<int>();
+
+            for (int i = 0; i < row.Elements.Count; ++i)
+            {
+                var cell = row.Elements[i] as RTFDomTableCell;
+                cell_strings.Add(GetCellText(cell));
+                cell_lengths.Add(Math.Max(i.ToString().Length, GetCellText(cell).Length));
+            }
+
+            Console.Write("┌");
+            first = true;
+            for (int i = 0; i < cell_strings.Count; ++i)
+            {
+                var length = cell_strings[i].Length;
+                
+                if (length == 0) continue;
+                
+                if (!first) Console.Write('┬');
+                Console.Write(new String('─', cell_lengths[i]));
+                first = false;
+     
+            }
+            Console.WriteLine('┐');
+
+
+            Console.Write("│");
+            first = true;
+            for (int i = 0; i < cell_strings.Count; ++i)
+            {
+                var length = cell_strings[i].Length;
+
+                if (length == 0) continue;
+
+                if (!first) Console.Write('│');
+                
+                if (i.ToString().Length < cell_lengths[i])
+                {
+                    string number_str = i.ToString().PadRight(cell_lengths[i]/2);
+                    Console.Write(number_str);
+                    Console.Write(new String(' ', cell_lengths[i] - number_str.Length));
+                }
+                else
+                {
+                    Console.Write(i.ToString());
+                }
+
+                first = false;
+
+            }
+            Console.WriteLine('│');
+
+            Console.Write("├");
+            first = true;
+            for (int i = 0; i < cell_strings.Count; ++i)
+            {
+                var length = cell_strings[i].Length;
+
+                if (length == 0) continue;
+
+                if (!first) Console.Write('┼');
+                Console.Write(new String('─', cell_lengths[i]));
+                first = false;
+
+            }
+            Console.WriteLine('┤');
+
+
+            Console.Write("│");
+            first = true;
+
+            for (int i = 0; i < cell_strings.Count; ++i)
+            {
+                var length = cell_strings[i].Length;
+                if (length == 0) continue;
+
+                if (!first) Console.Write("│");
+                if (length < cell_lengths[i])
+                {
+                    Console.Write(cell_strings[i].ToString());
+                    Console.Write(new String(' ', cell_lengths[i] - length));
+                }
+                else
+                {
+                    Console.Write(cell_strings[i].ToString());
+                }    
+                first= false;
+            }
+            Console.WriteLine("│");
+
+            Console.Write("└");
+            first = true;
+            for (int i = 0; i < cell_strings.Count; ++i)
+            {
+                var length = cell_strings[i].Length;
+
+                if (length == 0) continue;
+
+                if (!first) Console.Write('┴');
+                Console.Write(new String('─', cell_lengths[i]));
+                first = false;
+
+            }
+            Console.WriteLine('┘');
+
+        }
+    }
+
+
+    class Discipline_Row
+    {
+        public enum Fields
+        {
+            ID, 
+            NAME, 
+            SEM, 
+            STREAM, 
+            GROUPS, 
+            SUBGROUPS, 
+            STUDENTS, 
+            LEK,
+            PR,
+            LAB,
+            KUR,
+            IND,
+            SUM,
+
+            _COUNT
+        }
+
+        public static readonly Dictionary<Fields, int> fields_map = new Dictionary<Fields, int>()
+        {
+            { Fields.ID, 0 },
+            { Fields.NAME, 4 },
+            { Fields.SEM, 9 },
+            { Fields.STREAM, 12 },
+            { Fields.GROUPS, 15 },
+            { Fields.SUBGROUPS, 19 },
+            { Fields.STUDENTS, 22 },
+            { Fields.LEK, 25 },
+            { Fields.PR, 32 },
+            { Fields.LAB, 39 },
+            { Fields.KUR, 50 },
+            { Fields.IND, 50 },
+            { Fields.SUM, 54},
+        };
+
+        string [] data = new string[(int)Fields._COUNT];
+
+        string this[int index]
+        {
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
+
+        public string this[Fields index] 
+        {
+            get { return data[(int)index]; } 
+            set { data[(int)index] = value; }
+        }
+
+        public  int? id =>  Int32.TryParse(data[(int)Fields.ID], out var id) ? id: null;
+        public  string name => data[(int)Fields.NAME];
+        public  int? sem => Int32.TryParse(data[(int)Fields.SEM], out var s) ? s : null;
+        public  string stream => data[(int)Fields.STREAM];
+        public  int? group => Int32.TryParse(data[(int)Fields.GROUPS], out var g) ? g : null;
+        public  int? sub_group => Int32.TryParse(data[(int)Fields.SUBGROUPS], out var sg) ? sg : null;
+        public  int? student => Int32.TryParse(data[(int)Fields.STUDENTS], out var st) ? st : null;
+        public  double? lections => Double.TryParse(data[(int)Fields.LEK], out var l) ? l : null;
+        public  double? pr => Double.TryParse(data[(int)Fields.PR], out var p) ? p : null;
+        public  double? labs => Double.TryParse(data[(int)Fields.LAB], out var lab) ? lab : null;
+        public  double? ind => Double.TryParse(data[(int)Fields.IND], out var indv) ? indv : null;
+        public double? kur => Double.TryParse(data[(int)Fields.KUR], out var k) ? k : null;
+        public double? sum => Double.TryParse(data[(int)Fields.SUM], out var su) ? su : null;
+
+        public override string ToString()
+        {
+           //return  new string ($"{id} {name} {sem} {stream} {group} {sub_group} {student} {bud_student} {part} {lections} {pr} {labs} {ind} {kur} {sum}");
+           return data.Aggregate((a, b) => a + " " + b);
+        }
+
+
+        // навайбкодил
+        public void PrintFormatted()
+        {
+            // Вывод одной строки через общий метод, который выравнивает столбцы по всем строкам
+            PrintTable(new[] { this });
+        }
+
+        public static void PrintTable(IEnumerable<Discipline_Row> rowsEnumerable)
+        {
+            var rows = rowsEnumerable?.ToArray() ?? Array.Empty<Discipline_Row>();
+            if (rows.Length == 0) return;
+
+            // Получаем список полей в порядке объявления, исключая служебный _COUNT
+            var fields = Enum.GetValues(typeof(Fields)).Cast<Fields>().Where(f => f != Fields._COUNT).ToArray();
+
+            // Вычисляем ширину каждой колонки (максимум между именем поля и значением во всех строках)
+            var widths = new int[fields.Length];
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                var name = fields[i].ToString();
+                int maxValLen = 0;
+                foreach (var r in rows)
+                {
+                    var v = r.data[(int)fields[i]] ?? string.Empty;
+                    if (v.Length > maxValLen) maxValLen = v.Length;
+                }
+                widths[i] = Math.Max(name.Length, maxValLen);
+            }
+
+            // Верхняя граница
+            Console.Write('┌');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                if (i > 0) Console.Write('┬');
+                Console.Write(new string('─', widths[i] + 2));
+            }
+            Console.WriteLine('┐');
+
+            // Заголовок
+            Console.Write('│');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                var name = fields[i].ToString();
+                Console.Write(' ' + name.PadRight(widths[i]) + ' ');
+                Console.Write('│');
+            }
+            Console.WriteLine();
+
+            // Разделитель между заголовком и значениями
+            Console.Write('├');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                if (i > 0) Console.Write('┼');
+                Console.Write(new string('─', widths[i] + 2));
+            }
+            Console.WriteLine('┤');
+
+            // Строки данных
+            for (int rIndex = 0; rIndex < rows.Length; ++rIndex)
+            {
+                var r = rows[rIndex];
+                Console.Write('│');
+                for (int i = 0; i < fields.Length; ++i)
+                {
+                    var val = r.data[(int)fields[i]] ?? string.Empty;
+                    Console.Write(' ' + val.PadRight(widths[i]) + ' ');
+                    Console.Write('│');
+                }
+                Console.WriteLine();
+
+                // Если это не последняя строка, вывести разделитель между строками
+                if (rIndex + 1 < rows.Length)
+                {
+                    Console.Write('├');
+                    for (int i = 0; i < fields.Length; ++i)
+                    {
+                        if (i > 0) Console.Write('┼');
+                        Console.Write(new string('─', widths[i] + 2));
+                    }
+                    Console.WriteLine('┤');
+                }
+            }
+
+            // Нижняя граница
+            Console.Write('└');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                if (i > 0) Console.Write('┴');
+                Console.Write(new string('─', widths[i] + 2));
+            }
+            Console.WriteLine('┘');
+        }
+
+     
+    }
+
+    class VARow
+    {
+        public enum Fields
+        {
+            ID,
+            NAME,
+            SEM,
+            STREAM,
+            GROUPS,
+            STUDENTS,
+            HOURS_FORMULA,
+            HOURS,
+
+            _COUNT
+        }
+        public static readonly Dictionary<Fields, int> fields_map =  new Dictionary<Fields, int>()
+        { 
+            { Fields.ID, 0 },
+            { Fields.NAME, 4 },
+            { Fields.SEM, 7 },
+            { Fields.STREAM, 11 },
+            { Fields.GROUPS, 13 },
+            { Fields.STUDENTS, 18},
+            { Fields.HOURS_FORMULA, 22 },
+            { Fields.HOURS, 30 },           
+        };
+
+        string[] data = new string[(int)Fields._COUNT];
+
+        public string this[int index] 
+        { 
+            get { return data[index]; }
+            set { data[index] = value; }
+        }
+        public string this[Fields index]
+        {
+            get { return data[(int)index]; }
+            set { data[(int)index] = value; }
+        }
+
+        public override string ToString()
+        {
+            return data.Aggregate((a, b) => a + " " + b);
+        }   
+
+        public void PrintFormatted()
+        {
+            PrintTable(new[] { this });
+        }
+
+        public static void PrintTable(IEnumerable<VARow> rowsEnumerable)
+        {
+            var rows = rowsEnumerable?.ToArray() ?? Array.Empty<VARow>();
+            if (rows.Length == 0) return;
+
+            var fields = Enum.GetValues(typeof(Fields)).Cast<Fields>().Where(f => f != Fields._COUNT).ToArray();
+
+            var widths = new int[fields.Length];
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                var name = fields[i].ToString();
+                int maxValLen = 0;
+                foreach (var r in rows)
+                {
+                    var v = r.data[(int)fields[i]] ?? string.Empty;
+                    if (v.Length > maxValLen) maxValLen = v.Length;
+                }
+                widths[i] = Math.Max(name.Length, maxValLen);
+            }
+
+            // Верхняя граница
+            Console.Write('┌');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                if (i > 0) Console.Write('┬');
+                Console.Write(new string('─', widths[i] + 2));
+            }
+            Console.WriteLine('┐');
+
+            // Заголовок
+            Console.Write('│');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                var name = fields[i].ToString();
+                Console.Write(' ' + name.PadRight(widths[i]) + ' ');
+                Console.Write('│');
+            }
+            Console.WriteLine();
+
+            // Разделитель
+            Console.Write('├');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                if (i > 0) Console.Write('┼');
+                Console.Write(new string('─', widths[i] + 2));
+            }
+            Console.WriteLine('┤');
+
+            // Строки данных
+            for (int rIndex = 0; rIndex < rows.Length; ++rIndex)
+            {
+                var r = rows[rIndex];
+                Console.Write('│');
+                for (int i = 0; i < fields.Length; ++i)
+                {
+                    var val = r.data[(int)fields[i]] ?? string.Empty;
+                    Console.Write(' ' + val.PadRight(widths[i]) + ' ');
+                    Console.Write('│');
+                }
+                Console.WriteLine();
+
+                if (rIndex + 1 < rows.Length)
+                {
+                    Console.Write('├');
+                    for (int i = 0; i < fields.Length; ++i)
+                    {
+                        if (i > 0) Console.Write('┼');
+                        Console.Write(new string('─', widths[i] + 2));
+                    }
+                    Console.WriteLine('┤');
+                }
+            }
+
+            // Нижняя граница
+            Console.Write('└');
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                if (i > 0) Console.Write('┴');
+                Console.Write(new string('─', widths[i] + 2));
+            }
+            Console.WriteLine('┘');
+        }
+
+    }
+}
